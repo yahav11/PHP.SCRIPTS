13a9e50429647fd5d5ed3eaae665c8f8e1dd4bb1
yahav11 <122667557+yahav11@users.noreply.github.com>
2023-04-13 19:05:33 +0300

    Create SQLandPHP.php
    a script that connects php and SQL ,(no errores checking)

---
+++ SQLandPHP.php
@@ -0,0 +1,31 @@
+<?php
+// Database connection settings
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+// Fetch and display results
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// Close connection
+$conn->close();
+?>
